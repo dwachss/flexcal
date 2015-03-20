79c134f45848e28ec0e406f87927bae2852dd96e
Danny Wachsstock <d.wachss@prodigy.net>
2015-03-20 12:25:49 -0500

    create loadflexcal.js

---
+++ loadflexcal.js
@@ -0,0 +1,34 @@
+// loadflexcal.js
+/* This is far more complicated than is usually necessary. I just want this to be kept up to date with the
+	cutting-edge version, so it uses my jquery.repo routines to load.
+	Normally, just use
+		<link rel="stylesheet" href="flexcal.css" />
+	in your <head>, and include
+		<script src="jquery.ui.subclass.js"></script>
+		<script src="jquery.textpopup.js"></script>
+		<script src="jquery.flexcal.js"></script>
+	after including jQuery UI
+*/
+$.ajaxSetup({
+  cache: true // no reason not to cache scripts
+});
+$.getScript('https://cdn.rawgit.com/dwachss/jquery.repo/v1.1.1/jquery.repo.js').then(function(){
+	return $.repo('dwachss/flexcal');
+}).then(function (repo){
+	$('head').append('<link rel=stylesheet href="'+repo+'/flexcal.css" />');
+	return $.getScripts([
+		repo+'/jquery.ui.subclass.js',
+		repo+'/jquery.textpopup.js',
+		repo+'/jquery.flexcal.js'
+	]);
+}).then(function(){
+	return $.ready;
+}).then(function(){
+	return $.getScript ('javascripts/bililitedemo.js');
+});
+$.get('pages.json').then(function(pages){
+	for (page in pages){
+		$('<p>').append($('<a>').text(page).attr('href', pages[page])).appendTo('nav');
+	}
+});
+
