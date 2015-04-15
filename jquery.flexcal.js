b0552bd69e9462f0b4f16a682a19a66175c0b045
Danny Wachsstock <d.wachss@prodigy.net>
2015-04-15 16:26:12 -0500

    adjust styling to match jQuery UI datepicker

--- jquery.flexcal.js
+++ jquery.flexcal.js
@@ -203,6 +203,7 @@ $.widget('bililite.flexcal', $.bililite.textpopup, {
 	_adjustHTML: function(cal){
 		cal.find('a').removeClass('ui-state-focus').filter('.commit[rel="'+formatISO(this.options.current)+'"]').addClass('ui-state-focus');
 		cal.find('a').removeClass('ui-state-active').filter('.commit[rel="'+formatISO(this.parse(this.element.val()))+'"]').addClass('ui-state-active');
+		cal.find('a').removeClass('ui-state-highlight').filter('.commit[rel="'+formatISO(new Date)+'"]').addClass('ui-state-highlight');
 		cal.find('a:not([href])')['ui-clickable']();
 		cal.find('a.go').removeClass('ui-state-default') // ui-datepicker has its own styling
 			.each(function(){ this.title = $(this).text() }); // when we use image replacement for the prev/next buttons, leave the text as a tooltip title
